418cdeb41a75673ee47ad4e42dd04c3f3574591d
unknown <sebastiangotess@gmail.com>
2025-09-21 17:36:57 -0500

    Se empieza a hacer pruebas con los ejemplos del archivo txt readme, surgen dudas con la operación xor y rotación de bits

--- Desafio_1_Info_2_2025/main.cpp
+++ Desafio_1_Info_2_2025/main.cpp
@@ -14,6 +14,42 @@ int main() {
     else
         cout<<"Si se abrio"<<endl;
 
+    string contenido;
+    char cont;
+
+    while (archivo.get(cont)){
+        contenido += cont;
+    }
+
+    archivo.close();
+
+
+    char* letras = new char[contenido.size() + 1];
+
+    for (size_t i = 0;i < contenido.size();++i){
+        letras[i] = contenido[i];
+    }
+    letras[contenido.size()] = '\0';
+
+
+    for (size_t i = 0;i < contenido.size();++ i){
+        letras[i] = ((letras[i] ^ 0x5A) >> 3) | (letras[i] << (8 - 3));
+
+
+    }
+
+    for (size_t i = 0;i < contenido.size();++ i){
+        cout<<"Letra original: "<<static_cast<int>(contenido[i])<<", "<<"Letra convertida: "<<static_cast<int>(letras[i])<<endl;
+
+    }
+
+
+
+    cout<<endl;
+
+    delete[]letras;
+
+
     return 0;
 }
 
